1c6fcb382d1ee556d90bf85d960b5e50857e8401
Mariia <lebedevamv1@gmail.com>
2022-02-02 10:35:00 +0300

    full code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(45464);
+        int price = 10_000;
+        int rinmile = 20;
+        int bmile = price / rinmile;
+        System.out.println(bmile + " бонусных миль");
         // Объявляете переменные для входных данных и
         // параметров программы: одну для хранения
         // стоимости билета, другую для хранения количества
